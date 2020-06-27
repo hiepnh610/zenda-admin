d1ce75bc95879a229f3a18f0b5346c127a55d1f7
Hiep Nguyen <hiepnh610@gmail.com>
2020-06-27 16:00:59 +0700

    create chart for dashboard

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,14 +1,54 @@
 <template>
   <div class="container">
-    Dashboard
+    <chart :data="barChartData" :options="barChartOptions" :height="200" />
   </div>
 </template>
 
 <script>
+import Chart from '~/plugins/vue-chart'
+
 export default {
   name: 'Dashboard',
 
-  middleware: ['authenticated']
+  middleware: ['authenticated'],
+
+  components: {
+    Chart
+  },
+
+  data () {
+    return {
+      barChartData: {
+        labels: ['Jan', 'Feb', 'Mar'],
+        datasets: [
+          {
+            label: 'Points',
+            backgroundColor: ['#1ab394'],
+            data: [10, 15, 20]
+          }
+        ]
+      },
+      barChartOptions: {
+        responsive: true,
+        legend: {
+          display: false
+        },
+        title: {
+          display: true,
+          text: 'Top Receive'
+        },
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        }
+      }
+    }
+  }
 }
 </script>
 
